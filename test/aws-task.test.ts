a2f1c7a0b04f37a84ce944e83ae86f0e521bb530
Your Name <you@example.com>
2024-07-19 12:44:29 +0530

    Initial commit

---
+++ test/aws-task.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsTask from '../lib/aws-task-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-task-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsTask.AwsTaskStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
